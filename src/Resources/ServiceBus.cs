178fed30bb270d94b517136dbd3342f716413a9f
Charles Feval <chfeval@microsoft.com>
2019-08-21 12:08:24 -0700

    Adding service bus

---
+++ src/Resources/ServiceBus.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+
+namespace Provision
+{
+    internal class ServiceBus : IResource
+    {
+
+        public string Location { get; set; }
+        public string Name { get; set; } = "default";
+        public string SKU { get; set; } = "Basic";
+        [Dependency(Optional = true, Description = "If SKU is Premium, number of scale units")]
+        public string ScaleUnits { get; set; } = "1";
+        public string ServiceBusVariable { get; set; }
+        public string ServiceBusName { get; set; }
+        public string ConnectionStringVariable { get => ServiceBusVariable + "_CONNECTION_STRING"; }
+        public string KeyVariable { get => ServiceBusVariable + "_KEY"; }
+        public string KeyNameVariable { get => ServiceBusVariable + "_KEY_NAME"; }
+        public ResourceGroup ResourceGroup { get; set; }
+        public string[] Topics { get; set; } = new string[0];
+        [Dependency(Optional = true, Description = "Will create the subscriptions on the first topic")]
+        public string[] Subscriptions { get; set; } = new string[0];
+        public string[] Queues { get; set; } = new string[0];
+        public ServiceBus(Context context)
+        {
+            this.Location = $"${context.LocationVariable}";
+            if (string.IsNullOrEmpty(this.ServiceBusVariable))
+                this.ServiceBusVariable = Name.ToUpper() + "_SERVICE_BUS";
+            if (string.IsNullOrEmpty(this.ServiceBusName))
+                this.ServiceBusName = $"${context.BaseNameVariable}-${context.Random5charBaseVariable}";
+        }
+
+        public int Order { get; set; } = 2;
+    }
+}
---
+++ src/Resources/ServiceBusGenerator.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Provision
+{
+    internal class ServiceBusGenerator : IResourceGenerator
+    {
+        private ServiceBus serviceBus;
+        public ServiceBusGenerator(ServiceBus serviceBus)
+        {
+            this.serviceBus = serviceBus;
+        }
+
+        public string GenerateCleanupScript() => "";
+
+        private string GenerateTopics() =>
+            serviceBus.Topics.Length > 0 ? "\n" + string.Join("\n",
+                serviceBus.Topics.Select((System.Func<string, string>)(
+                    topicName => $@"echo ""Creating topic ${serviceBus.ServiceBusVariable}.{topicName}""
+az servicebus topic create -g ""${serviceBus.ResourceGroup.ResourceGroupNameVariable}"" --name ""{topicName}"" --namespace-name ${serviceBus.ServiceBusVariable} --query ""created"" -o tsv")))
+                : "";
+
+        private string GenerateQueues() =>
+            serviceBus.Queues.Length > 0 ? "\n" + string.Join("\n",
+                serviceBus.Queues.Select((System.Func<string, string>)(
+                    queueName => $@"echo ""Creating queue ${serviceBus.ServiceBusVariable}.{queueName}""
+az servicebus queue create -g ""${serviceBus.ResourceGroup.ResourceGroupNameVariable}"" --name ""{queueName}"" --namespace-name ${serviceBus.ServiceBusVariable} --query ""created"" -o tsv")))
+                : "";
+
+        private string GenerateSubscriptions() =>
+            serviceBus.Subscriptions.Length > 0 ? "\n" + string.Join("\n",
+                serviceBus.Subscriptions.Select((System.Func<string, string>)(
+                    subscription => $@"echo ""Creating subscription ${serviceBus.ServiceBusVariable}.{subscription}""
+az servicebus topic subscription create -g ""${serviceBus.ResourceGroup.ResourceGroupNameVariable}"" --name ""{subscription}"" --namespace-name ${serviceBus.ServiceBusVariable} --topic-name {serviceBus.Topics[0]} --query ""created"" -o tsv")))
+            : "";
+
+        public string GenerateProvisioningScript() => $@"echo ""Creating service bus namespace ${serviceBus.ServiceBusVariable}""
+az servicebus namespace create --name ${serviceBus.ServiceBusVariable} --sku {serviceBus.SKU} --location {serviceBus.Location} -g ${serviceBus.ResourceGroup.ResourceGroupNameVariable} --query ""provisioningState"" -o tsv
+{serviceBus.KeyNameVariable}=""RootManageSharedAccessKey""
+IFS=$'\t' read -r {serviceBus.ConnectionStringVariable} {serviceBus.KeyVariable} <<< `az servicebus namespace authorization-rule keys list -g ""${serviceBus.ResourceGroup.ResourceGroupNameVariable}"" --namespace-name ""${serviceBus.ServiceBusVariable}"" -n ""${serviceBus.KeyNameVariable}"" -o tsv --query ""{{connectionString: primaryConnectionString, primaryKey: primaryKey}}""`"
+            + GenerateTopics() + GenerateQueues() + GenerateSubscriptions();
+        public string GenerateResourceNameDeclaration() => $@"{serviceBus.ServiceBusVariable}=""{serviceBus.ServiceBusName}""";
+
+        public string GenerateSummary() => $@"echo ""  Service bus   ({serviceBus.Name}): ${serviceBus.ServiceBusVariable}""
+echo ""      Key name ({serviceBus.Name}): ${serviceBus.KeyNameVariable}""
+echo ""      Key value ({serviceBus.Name}): ${serviceBus.KeyVariable}""
+echo ""      Connection string ({serviceBus.Name}): ${serviceBus.ConnectionStringVariable}""";
+
+        public string GenerateEnvScript() => $@"IFS=$'\t' read -r {serviceBus.ConnectionStringVariable} {serviceBus.KeyVariable} <<< `az servicebus namespace authorization-rule keys list -g ""${serviceBus.ResourceGroup.ResourceGroupNameVariable}"" --namespace-name ""${serviceBus.ServiceBusVariable}"" -n ""${serviceBus.KeyNameVariable}"" -o tsv --query ""{{connectionString: primaryConnectionString, primaryKey: primaryKey}}""`";
+    }
+}
