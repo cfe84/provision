e8b79e7b2a4401d3b595e4dbe3549aceee6e42e2
Charles Feval <chfeval@microsoft.com>
2019-10-29 15:47:43 -0700

    Fixing default name

--- src/Resources/Disk.cs
+++ src/Resources/Disk.cs
@@ -1,31 +1,39 @@
 using System.Collections.Generic;
 
-namespace Provision {
-    internal class Disk: IResource {
-        
-        public string Location {get; set;}
-        public string Name {get; set;} = "default";
-        public string DiskName {get; set;}
+namespace Provision
+{
+    internal class Disk : IResource
+    {
+
+        public string Location { get; set; }
+        public string Name { get; set; } = "default";
+        private string diskName = null;
+        public string DiskName
+        {
+            get => diskName ?? $"${context.BaseNameVariable}-{Name}";
+            set => diskName = value;
+        }
         private string diskVariableName = null;
-        public string DiskVariableName {
+        public string DiskVariableName
+        {
             get => diskVariableName ?? this.Name.ToUpper() + "_DISK";
             set => diskVariableName = value;
         }
         [Dependency(Optional = true, Description = "Can be either of Windows or Linux, default is Windows")]
         public string OsType { get; set; } = "Windows";
-        public string DiskResourceIdVariableName {
+        public string DiskResourceIdVariableName
+        {
             get => DiskVariableName + "_RESOURCE_ID";
         }
         public string SKU { get; set; } = "Standard_LRS";
-        public string SizeGb {get; set;} = "20";
-        public ResourceGroup ResourceGroup {get; set;}
-        
+        public string SizeGb { get; set; } = "20";
+        public ResourceGroup ResourceGroup { get; set; }
+
         public Disk(Context context)
         {
             this.Location = $"${context.LocationVariable}";
-            this.DiskName = $"${context.BaseNameVariable}-{Name}";
         }
 
-        public int Order {get ; set; } = 2;
+        public int Order { get; set; } = 2;
     }
 }
